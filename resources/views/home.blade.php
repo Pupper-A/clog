d5f97a3190153c23e232aba395bc19626a6e75d9
leyla <dev.leyla.sabouri@gmail.com>
2022-08-10 06:23:54 +0300

    remove layout. blade and some changes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,15 +1,5 @@
 @extends('home-layout')
-@section('colorful')
-    <div class="ui text container">
-        <div class="ui  header">
-            <h2 class="ui  header" style="font-family:initial; color:lightgray;font-size:1.5em;">JUST DONT TRUST ANY CONTENT
-                YOU READ ONLINE</h2>
-            <div class="ui huge primary button" style="margin-top:18px; background-color:mediumorchid;">Get Started</div>
-        </div>
-    </div>
-@endsection
-
 @section('content')
     <div class="row justify-content-center">
-        <div class="col-md-6" style="margin-top:50px;">
-        @endsection
+    </div>
+@endsection
--- resources/views/memo.blade.php
+++ resources/views/memo.blade.php
@@ -1,49 +1,51 @@
 <?php use carbon\carbon; ?>
-@extends ('layout')
-@section('title', 'Memos')
-@section('card-body')
-    <form action="{{ route('memo.store') }}" method="post">
-        @csrf
-        <div class="form-group">
-            <label for="memo">enter memo</label>
-
-            <textarea id="memo" class="form-control" name="memo" rows="4" cols="50"></textarea>
+@extends('home-layout')
+@section('content')
+    <div class="container">
+        <div class="row mt-5 justify-content-center">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h5>Create Memo</h5>
+                    </div>
+                    <div class="card-body">
+                        <form action="{{ route('memo.store') }}" method="post">
+                            @csrf
+                            <div class="form-group">
+                                <label for="memo">Share Memory</label>
+                                <textarea id="memo" class="form-control" name="memo" rows="4" cols="180"></textarea>
+                            </div>
+                            <input type="submit" class="btn btn-success mt-6" name="submit" value="Send" />
+                        </form>
+                    </div>
+                </div>
+                <div class=" table-responsive mt-5">
+                    @if (count($memos) > 0)
+                        <table class="table table-striped table-bordered table-hover">
+                            <thead>
+                                <tr>
+                                    <th>memo</th>
+                                    <th>created time</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($memos as $memo)
+                                    <tr>
+                                        <td>{{ $memo->content }}</td>
+                                        <td>
+                                            @if ($memo->created_at > carbon::now()->format('Y-m-d 00:00:00'))
+                                                {{ $memo->created_at->diffForHumans() }}
+                                            @else
+                                                {{ $memo->created_at }}
+                                            @endif
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    @endif
+                </div>
+            </div>
         </div>
-
-        <input type="submit" class="btn btn-primary mt-6" name="submit" value="submit" value="add" />
-
-    </form>
+    </div>
 @endsection
-
-@section('show_memos')
-    @if (count($memos) > 0)
-        <table class="table table-striped table-bordered table-hover">
-            <thead>
-                <tr>
-                    <th>memo</th>
-                    <th>created time</th>
-
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($memos as $memo)
-                    <tr>
-                        <td>{{ $memo->content }}</td>
-                        <td>
-                            @if ($memo->created_at > carbon::now()->format('Y-m-d 00:00:00'))
-                                {{ $memo->created_at->diffForHumans() }}
-                            @else
-                                {{ $memo->created_at }}
-                            @endif
-                        </td>
-
-                    </tr>
-                @endforeach
-
-            </tbody>
-
-        </table>
-    @endif
-
-@endsection
-
